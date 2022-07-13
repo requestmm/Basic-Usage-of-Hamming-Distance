0fb0bd5fd143a6f93323d26be5d62c9a6e0c5011
requestmm <94811774+requestmm@users.noreply.github.com>
2022-07-12 22:57:29 -0400

    Create example_hamming_distance.py

---
+++ example_hamming_distance.py
@@ -0,0 +1,26 @@
+# Basic usage of Hamming Distance to analyse cities wrote on XLSX file.
+
+
+import pandas as pd
+from scipy.spatial.distance import hamming
+
+df = pd.read_excel(R"cities_MG_Brazil.xlsx")
+
+target_word = "Água Boa"
+target_word_length = len(target_word)
+
+for reference_word in df["CITY_NAME"]:
+    reference_word_length = len(reference_word)
+    diff = target_word_length - reference_word_length
+    if diff > 0:
+        reference_word = reference_word.ljust(diff, ' ')
+    elif diff < 0:
+        reference_word = reference_word[:diff]
+
+    hamming_distance = hamming(target_word, reference_word)
+
+    print(target_word, reference_word, hamming_distance)
+
+
+
+
