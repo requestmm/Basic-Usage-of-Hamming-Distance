f6938c9a6a4fbe30862e8880c66bb4f875d289e1
requestmm <94811774+requestmm@users.noreply.github.com>
2022-07-12 23:13:20 -0400

    fix: length of reference word

--- example_hamming_distance.py
+++ example_hamming_distance.py
@@ -13,14 +13,14 @@ for reference_word in df["CITY_NAME"]:
     reference_word_length = len(reference_word)
     diff = target_word_length - reference_word_length
     if diff > 0:
-        reference_word = reference_word.ljust(diff, ' ')
+        reference_word = reference_word = reference_word + "_" * diff
     elif diff < 0:
         reference_word = reference_word[:diff]
+
+    target_word = list(target_word)
+    reference_word = list(reference_word)
+    
 
     hamming_distance = hamming(target_word, reference_word)
 
     print(target_word, reference_word, hamming_distance)
-
-
-
-
